9d412221ab3935632b3cc80adeb03ff306a6678a
kermage <genealyson.torcende@gmail.com>
2023-01-28 00:11:22 +0800

    transform control args with wanted property keys and only supported types

--- src/CustomField.php
+++ src/CustomField.php
@@ -29,7 +29,44 @@ class CustomField extends Base {
 	public function hook( WP_Customize_Manager $customizer ): void {
 
 		$customizer->add_setting( $this->get_identifier(), array_merge( $this->config, array( 'type' => 'theme_mod' ) ) );
-		$customizer->add_control( $this->get_identifier(), $this->config );
+		$customizer->add_control( $this->get_identifier(), $this->transform( $this->config ) );
+
+	}
+
+
+	protected function transform( array $config ): array {
+
+		$keys = array(
+			'label'   => 'title',
+			'choices' => 'options',
+		);
+
+		foreach ( $keys as $expected => $provided ) {
+			$config[ $expected ] = $config[ $provided ];
+
+			unset( $config[ $provided ] );
+		}
+
+		$supported = array(
+			'text',
+			'email',
+			'url',
+			'number',
+			'hidden',
+			'date',
+			'checkbox',
+			'textarea',
+			'radio',
+			'select',
+			'dropdown-pages',
+			'color',
+		);
+
+		if ( ! in_array( $config['type'], $supported, true ) ) {
+			$config['type'] = 'text';
+		}
+
+		return $config;
 
 	}
 
